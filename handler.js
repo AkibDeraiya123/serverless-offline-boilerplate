71885bbe52be1bd2f2c38473d804538915eddde7
malik lakhani <maliklakhani67@gmail.com>
2018-06-21 17:11:47 +0530

    server less with dynamodb local

--- handler.js
+++ handler.js
@@ -1,16 +1,32 @@
 'use strict';
+var AWS = require('aws-sdk');
+const dynamodb =new AWS.DynamoDB.DocumentClient({
+    region: 'localhost',
+    endpoint: 'http://localhost:8000'
+})
 
 module.exports.hello = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
+
+  var params = {
+    TableName: "usersTable"
   };
 
-  callback(null, response);
+ dynamodb.scan(params, function(err, data) {
+   if (err) {
+    console.log(err, err.stack);
+  }
+   else {
+    console.log(data);
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'Go Serverless v1.0! Your function executed successfully!',
+        data: data,
+      }),
+    };
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // callback(null, { message: 'Go Serverless v1.0! Your function executed successfully!', event });
+    callback(null, response);
+   }
+ });
+
 };
